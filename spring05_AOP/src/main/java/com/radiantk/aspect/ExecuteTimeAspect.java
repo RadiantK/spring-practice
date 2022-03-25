fa54f213df9ad77a1618d09a67af7bb53fa17cf7
RadiantK <pps8853@gmail.com>
2022-03-25 09:23:53 +0900

    AOP클래스 구현
    메소드 실행 전/후로 동작할 AOP클래스를 생성함.
    코드는 간단히 메소드의 실행 시간을 출력

---
+++ spring05_AOP/src/main/java/com/radiantk/aspect/ExecuteTimeAspect.java
@@ -0,0 +1,36 @@
+package com.radiantk.aspect;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class ExecuteTimeAspect {
+
+	// com.radiantk.test패키지와 그 하위 패키지에 위치한 타입의 public 메서드를 지정
+	@Pointcut("execution(public* com.radiantk.test..*(..))")
+	private void cut() {}
+	
+	// ProceedingJoinPoint 프록시 대상 객체의 메소드를 호출할 때 사용
+	@Around("cut()")
+	public Object measure(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.nanoTime();
+		
+		try {
+			Object result = joinPoint.proceed(); // 메소드를 호출할 때 사용
+			System.out.println("result : " + result);
+			return result;
+		}  finally {
+			long finish = System.nanoTime();
+			Signature sig = joinPoint.getSignature();
+			System.out.printf("%s.%s(%s) 실행 시간 : %d ns\n",
+					joinPoint.getTarget().getClass().getSimpleName(),
+					sig.getName(), Arrays.toString(joinPoint.getArgs()),
+					(finish - start));
+		}
+	}
+}
