5037fa50b3c4dc28af5cb86ac4e9bbbcb5229ac2
RadiantK <pps8853@gmail.com>
2022-04-03 22:55:20 +0900

    DB설정 클래스 수정
    유휴(가동되지 않는) 커넥션을 검사하는 메소드 추가

--- spring06_DBConnection/src/main/java/com/radiantk/config/DbConfig.java
+++ spring06_DBConnection/src/main/java/com/radiantk/config/DbConfig.java
@@ -16,6 +16,9 @@ public class DbConfig {
 		ds.setPassword("spring5");
 		ds.setInitialSize(2); // 초기 커넥션 개수(기본값 10)
 		ds.setMaxActive(10); // 최대 커넥션 개수(기본값 100)
+		ds.setTestWhileIdle(true); // 가동되지않는 커넥션 검사(유휴 커넥션)
+		ds.setMinEvictableIdleTimeMillis(60000 * 3); // 최소 가동되지 않는 시간 3분
+		ds.setTimeBetweenEvictionRunsMillis(1000 * 10); // 10초 주기로 유휴 검사
 		
 		return ds;
 	}
