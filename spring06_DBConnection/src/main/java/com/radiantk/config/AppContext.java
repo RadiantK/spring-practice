220768fec51bf9077213f3f8e5ebbe71da8cf697
RadiantK <pps8853@gmail.com>
2022-04-03 23:26:09 +0900

    AppContext 수정
    스프링 빈으로 DataSource 등록 후 MemberDao에 DataSource 주입

--- spring06_DBConnection/src/main/java/com/radiantk/config/AppContext.java
+++ spring06_DBConnection/src/main/java/com/radiantk/config/AppContext.java
@@ -1,5 +1,7 @@
 package com.radiantk.config;
 
+
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +14,26 @@ import com.radiantk.spring.MemberRegisterService;
 
 @Configuration
 public class AppContext {
+	
+	@Bean(destroyMethod = "close") // 커넥션 풀에 보관된 커넥션을 닫음
+	public DataSource dataSource() {
+		DataSource ds = new DataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver"); // JDBC 드라이버 클래스 지정
+		ds.setUrl("jdbc:mysql://localhost/spring5?characterEncoding=utf8"); // jdbc url지정
+		ds.setUsername("spring5");
+		ds.setPassword("spring5");
+		ds.setInitialSize(2); // 초기 커넥션 개수(기본값 10)
+		ds.setMaxActive(10); // 최대 커넥션 개수(기본값 100)
+		ds.setTestWhileIdle(true); // 가동되지않는 커넥션 검사(유휴 커넥션)
+		ds.setMinEvictableIdleTimeMillis(60000 * 3); // 최소 가동되지 않는 시간 3분
+		ds.setTimeBetweenEvictionRunsMillis(1000 * 10); // 10초 주기로 유휴 검사
+		
+		return ds;
+	}
 
 	@Bean
 	public MemberDao memberDao() {
-		return new MemberDao();
+		return new MemberDao(dataSource());
 	}
 	
 	@Bean
