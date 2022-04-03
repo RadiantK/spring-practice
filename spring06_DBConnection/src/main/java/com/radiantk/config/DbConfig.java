ca0fed3f09458894456af3c2669411d4621cb8f3
RadiantK <pps8853@gmail.com>
2022-04-03 21:52:31 +0900

    DB설정 클래스 수정
    @Bean설정 변경 및 주석 추가

--- spring06_DBConnection/src/main/java/com/radiantk/config/DbConfig.java
+++ spring06_DBConnection/src/main/java/com/radiantk/config/DbConfig.java
@@ -1,19 +1,21 @@
 package com.radiantk.config;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DbConfig {
 	
+	@Bean(destroyMethod = "close") // 커넥션 풀에 보관된 커넥션을 닫음
 	public DataSource dataSource() {
 		DataSource ds = new DataSource();
 		ds.setDriverClassName("com.mysql.jdbc.Driver"); // JDBC 드라이버 클래스 지정
 		ds.setUrl("jdbc:mysql://localhost/spring5?characterEncoding=utf8"); // jdbc url지정
 		ds.setUsername("spring5");
 		ds.setPassword("spring5");
-		ds.setInitialSize(2);
-		ds.setMaxActive(10);
+		ds.setInitialSize(2); // 초기 커넥션 개수(기본값 10)
+		ds.setMaxActive(10); // 최대 커넥션 개수(기본값 100)
 		
 		return ds;
 	}
