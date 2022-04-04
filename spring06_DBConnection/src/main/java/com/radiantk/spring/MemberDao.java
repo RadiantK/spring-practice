5789db78388ba418dc15768e41932832484998a4
RadiantK <pps8853@gmail.com>
2022-04-04 20:54:50 +0900

    MemberDao수정
    selectByEmail메소드를 jdbcTemplate을 사용해서 변경

--- spring06_DBConnection/src/main/java/com/radiantk/spring/MemberDao.java
+++ spring06_DBConnection/src/main/java/com/radiantk/spring/MemberDao.java
@@ -1,11 +1,44 @@
 package com.radiantk.spring;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class MemberDao {
 
-	public Member selectByEmail(String email) {
-		return null;
+	private JdbcTemplate jdbcTemplate;
+	
+	public  MemberDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	Member selectByEmail(String email) {
+		String sql = "SELECT * FROM MEMBER WHERE EMAIL = ?";
+		// jdbcTemplate.query(String sql, RowMapper<T> rowMapper, Objects... args)
+		// 인덱스기반 파라미터를 가진 쿼리문(?)이면 args를 이용해서 파라미터 값 지정
+		List<Member> results = jdbcTemplate.query(sql, 
+			new RowMapper<Member>() {
+				// ResultSet의 데이터를 읽어옴, rowNum : 현재 행의 번호
+				@Override
+				public Member mapRow(ResultSet rs, int rowNum) 
+						throws SQLException {
+					Member member = new Member(
+							rs.getString("email"),
+							rs.getString("password"),
+							rs.getString("name"),
+							rs.getTimestamp("regDate").toLocalDateTime());
+					member.setId(rs.getLong("id"));
+					return member;
+				}
+			}, email); // email이 ?에 매핑됨
+		
+		return results.isEmpty() ? null : results.get(0);
 	}
 	
 	public void insert(Member member) {
