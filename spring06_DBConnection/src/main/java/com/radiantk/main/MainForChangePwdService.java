1830a2d246afd9d6ef8c9e878b372ec0cf2d8c4c
RadiantK <pps8853@gmail.com>
2022-04-08 22:16:49 +0900

    트랜잭션 commit, rollback 로그확인
    트랜잭션의 commit, rollback 확인

--- spring06_DBConnection/src/main/java/com/radiantk/main/MainForChangePwdService.java
+++ spring06_DBConnection/src/main/java/com/radiantk/main/MainForChangePwdService.java
@@ -17,7 +17,7 @@ public class MainForChangePwdService {
 		
 		try {
 			cps.ChangePassword(
-					"radiantk@radiantk.com", "1234", "1111");
+					"radiantk@radiantk.com", "12345", "11111");
 			System.out.println("암호를 변경했습니다.");
 		} catch(MemberNotFoundException e) {
 			System.out.println("회원 데이터가 존재하지 않습니다.");
