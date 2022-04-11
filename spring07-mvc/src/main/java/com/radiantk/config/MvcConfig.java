ba7b15cef074ba465b22055051881e18d0141ef3
RadiantK <pps8853@gmail.com>
2022-04-11 23:10:00 +0900

    MVC설정 클래스 생성
    dispatcher 설정메소드와
    viewResolver설정메소드를 구현하고 빈으로 등록

---
+++ spring07-mvc/src/main/java/com/radiantk/config/MvcConfig.java
@@ -0,0 +1,26 @@
+package com.radiantk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// WebMvcConfigurer 인터페이스 : 스프링 MVC의 개별설정을 조정할 때 사용
+@Configuration // 스프링 설정 클래스
+@EnableWebMvc // 스프링 MVC를 구성할 때 필요한 빈의 설정을 자동으로 해줌(빈의 등록을 손쉽게 할 수 있음)
+public class MvcConfig implements WebMvcConfigurer {
+
+	// DispatcherServlet의 매핑경로를'/'로 주었을 때
+	// JSP/HTML/CSS 등을 올바르게 처리하기 위한 설정을 추가한다.
+	@Override
+	public void configureDefaultServletHandling(
+			DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	// JSP를 이용해서 컨트롤러의 실행결과를 보여주기 위한 설정을 추가한다.
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+}
