2e6fc91451b0cafa0f2279a9c25c1e2c45c3fd5c
RadiantK <pps8853@gmail.com>
2022-04-14 23:56:47 +0900

    커맨드 객체로 사용할 DTO생성
    설문 응답자 정보를 담는 DTO 생성

---
+++ spring08-mvc01/src/main/java/com/radiantk/survey/Respondent.java
@@ -0,0 +1,25 @@
+package com.radiantk.survey;
+
+// 응답자 정보
+public class Respondent {
+	
+	private int age;
+	private String location;
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+}
