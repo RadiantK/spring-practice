cceed5a42c4d2aaf1c256a0f6733adf26de3bcbd
RadiantK <pps8853@gmail.com>
2022-04-14 16:54:45 +0900

    Update RegisterController.java
    step2의 Post요청을 담당하는 컨트롤러 메소드에서 step2.jsp 페이지에서 사용할 커맨드 객체를 모델에 넣어줌

--- spring08-mvc01/src/main/java/com/radiantk/controller/RegisterController.java
+++ spring08-mvc01/src/main/java/com/radiantk/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package com.radiantk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,10 +28,13 @@ public class RegisterController {
 	
 	@PostMapping("/register/step2")
 	public String handleStep2(
-			@RequestParam(value = "agree", defaultValue = "false") Boolean agree) {
+			@RequestParam(value = "agree", defaultValue = "false") Boolean agree,
+			Model model) {
 		if(!agree) {
 			return "register/step1";
 		}
+		// <form:form>태그에서 사용할 커맨드 객체를 모델에 넣어줌
+		model.addAttribute("registerRequest", new RegisterRequest());
 		return "register/step2";
 	}
 	
@@ -42,6 +46,8 @@ public class RegisterController {
 	// 스프링은 요청 파라미터값을 커멘드 객체에 담아주는 기능을 한다.
 	// 예를들어 이름이 name인 요청 파라미터 값을 커맨드 객체의 setName() 메소드를 사용해서
 	// 커맨드 객체에 전달하는 기능을 제공한다.
+	// 커맨드 객체는 보통 DTO 를 의미하며, HttpServletRequest 로 받아오는 요청 파라미터의
+	// key 값과 동일한 이름의 속성들과 setter 메서드를 가지고 있어야 한다.
 	@PostMapping("/register/step3")
 	public String handleStep3(RegisterRequest regReq) {
 		try {
