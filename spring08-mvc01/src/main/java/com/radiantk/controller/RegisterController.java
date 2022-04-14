b1cf86820c7655e2e3bbedacabbfb58ac5302e2b
RadiantK <pps8853@gmail.com>
2022-04-14 14:27:17 +0900

    step3 페이지 컨트롤러 생성
    회원을 등록하면 이동하는 step3페이지의 컨트롤러를 구현함.
    스프링의 기능으로 parameter를 커맨드 객체로 받아서 사용

--- spring08-mvc01/src/main/java/com/radiantk/controller/RegisterController.java
+++ spring08-mvc01/src/main/java/com/radiantk/controller/RegisterController.java
@@ -6,9 +6,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.radiantk.spring.DuplicateMemberException;
+import com.radiantk.spring.MemberRegisterService;
+import com.radiantk.spring.RegisterRequest;
+
 @Controller
 public class RegisterController {
 
+	private MemberRegisterService memberRegisterService;
+	
+	public void setMemberRegisterService(
+			MemberRegisterService memberRegisterService) {
+		this.memberRegisterService = memberRegisterService;
+	}
+	
 	@RequestMapping("/register/step1")
 	public String handleStep1() {
 		return "register/step1";
@@ -26,5 +37,18 @@ public class RegisterController {
 	@GetMapping("/register/step2")
 	public String handleStep2Get() {
 		return "redirect:/register/step1";
+	}
+	
+	// 스프링은 요청 파라미터값을 커멘드 객체에 담아주는 기능을 한다.
+	// 예를들어 이름이 name인 요청 파라미터 값을 커맨드 객체의 setName() 메소드를 사용해서
+	// 커맨드 객체에 전달하는 기능을 제공한다.
+	@PostMapping("/register/step3")
+	public String handleStep3(RegisterRequest regReq) {
+		try {
+			memberRegisterService.regist(regReq);
+			return "register/step3";
+		}catch(DuplicateMemberException e) {
+			return "register/step2";
+		}
 	}
 }
