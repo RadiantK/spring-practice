7bd72527646b603e61f25d405ad96467b483d264
RadiantK <pps8853@gmail.com>
2022-04-17 12:35:56 +0900

    form:form 커스텀 태그들 사용
    form:form 커스텀 태그들을 사용하기 위한 클래스 생성

---
+++ spring08-mvc01/src/main/java/com/radiantk/form/Code.java
@@ -0,0 +1,21 @@
+package com.radiantk.form;
+
+public class Code {
+
+	private String code;
+	private String label;
+
+	public Code(String code, String label) {
+		this.code = code;
+		this.label = label;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+}
