50c1064ba0f3cef68f262efa6f9a64a06a77cd11
RadiantK <pps8853@gmail.com>
2022-04-18 20:52:48 +0900

    RegisterController 수정
    에러 메시지를 담는 Errors객체를 파라미터로 추가함

--- spring09-mvc02/src/main/java/com/radiantk/controller/RegisterController.java
+++ spring09-mvc02/src/main/java/com/radiantk/controller/RegisterController.java
@@ -2,6 +2,7 @@ package com.radiantk.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,16 +48,19 @@ public class RegisterController {
 	}
 	
 	// 스프링은 요청 파라미터값을 커멘드 객체에 담아주는 기능을 한다.
-	// 예를들어 이름이 name인 요청 파라미터 값을 커맨드 객체의 setName() 메소드를 사용해서
-	// 커맨드 객체에 전달하는 기능을 제공한다.
 	// 커맨드 객체는 보통 DTO 를 의미하며, HttpServletRequest 로 받아오는 요청 파라미터의
 	// key 값과 동일한 이름의 속성들과 setter 메서드를 가지고 있어야 한다.
 	@PostMapping("/register/step3")
-	public String handleStep3(RegisterRequest regReq) {
+	public String handleStep3(RegisterRequest regReq, Errors errors) {
+		new RegisterRequestValidator().validate(regReq, errors);
+		if(errors.hasErrors()) { // 에러가 존재하는지 검사
+			return "register/step2";
+		}
 		try {
 			memberRegisterService.regist(regReq);
 			return "register/step3";
 		}catch(DuplicateMemberException e) {
+			errors.rejectValue("email", "duplicate");
 			return "register/step2";
 		}
 	}
