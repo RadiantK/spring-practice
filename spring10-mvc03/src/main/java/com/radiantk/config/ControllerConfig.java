6b266e3914a2510deaef27fbad7eb90aedddeadb
RadiantK <pps8853@gmail.com>
2022-04-25 13:58:22 +0900

    ChangePasswordController를 빈으로 등록
    ChangePasswordController를 빈으로 등록하고 changePasswordService를 set메소드를 통해 주입.

--- spring10-mvc03/src/main/java/com/radiantk/config/ControllerConfig.java
+++ spring10-mvc03/src/main/java/com/radiantk/config/ControllerConfig.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.radiantk.controller.ChangePasswordController;
 import com.radiantk.controller.LoginController;
 import com.radiantk.controller.LogoutController;
 import com.radiantk.controller.RegisterController;
 import com.radiantk.spring.AuthService;
+import com.radiantk.spring.ChangePasswordService;
 import com.radiantk.spring.MemberRegisterService;
 
 @Configuration
@@ -17,6 +19,8 @@ public class ControllerConfig {
 	public MemberRegisterService memberRegisterService;
 	@Autowired
 	private AuthService authService;
+	@Autowired
+	private ChangePasswordService changePasswordService;
 	
 	@Bean
 	public RegisterController registerController() {
@@ -35,5 +39,12 @@ public class ControllerConfig {
 	@Bean
 	public LogoutController logoutController() {
 		return new LogoutController();
+	}
+	
+	@Bean
+	public ChangePasswordController changePasswordController() {
+		ChangePasswordController controller = new ChangePasswordController();
+		controller.setChangePasswordSerivce(changePasswordService);
+		return controller;
 	}
 }
