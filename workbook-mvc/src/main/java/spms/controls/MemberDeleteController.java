62948df0b7901551a15f4ca7900ab3381da88e90
RadiantK <pps8853@gmail.com>
2022-05-09 23:09:00 +0900

    회원을 제거하는 컨트롤러
    회원을 제거하는 컨트롤러

---
+++ workbook-mvc/src/main/java/spms/controls/MemberDeleteController.java
@@ -0,0 +1,36 @@
+package spms.controls;
+
+import java.util.Map;
+
+import spms.annotation.Component;
+import spms.bind.DataBinding;
+import spms.dao.MemberDao;
+
+@Component("/member/delete.do")
+public class MemberDeleteController implements Controller, DataBinding {
+
+	MemberDao memberDao;
+	
+	public MemberDeleteController setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+		return this;
+	}
+	
+	@Override
+	public Object[] getDataBinders() {
+		return new Object[] {
+			"no", Integer.class	
+		};
+	}
+	
+	@Override
+	public String execute(Map<String, Object> model) throws Exception {
+		
+//		MemberDao memberDao = (MemberDao)model.get("memberDao");
+		Integer no = (Integer)model.get("no");
+		memberDao.delete(no);
+		
+		return "redirect:list.do";
+	}
+	
+}
