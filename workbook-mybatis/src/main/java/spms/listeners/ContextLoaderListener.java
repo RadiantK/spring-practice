189cc4d75abe3d8c4350c92c4c020720fba3946c
RadiantK <pps8853@gmail.com>
2022-05-15 17:39:11 +0900

    contextLoaderListener수정
    웹어플리케이션 시작및 종료시 이벤트를 처리하는 contextLoaderListener수정

--- workbook-mybatis/src/main/java/spms/listeners/ContextLoaderListener.java
+++ workbook-mybatis/src/main/java/spms/listeners/ContextLoaderListener.java
@@ -1,9 +1,15 @@
 package spms.listeners;
 
+import java.io.InputStream;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import spms.context.ApplicationContext;
 
@@ -21,13 +27,27 @@ public class ContextLoaderListener implements ServletContextListener{
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
 		try {
+			applicationContext = new ApplicationContext();
+			// mybatis 설정 파일(보통 자바 클래스 경로에 둠)
+			String resource = "spms/dao/mybatis-config.xml";
+			// getResourceAsStream() 자바 클래스 경로에 있는 파일의 입력스트림을 얻을 수 있음
+			InputStream is = Resources.getResourceAsStream(resource);
+			// mysql 설정 파일을 토대로 SqlSessionFactory 생성(Builder pattern)
+			SqlSessionFactory sqlSessionFactory = 
+					new SqlSessionFactoryBuilder().build(is); 
+			
+			applicationContext.addBean("sqlSessionFactory", sqlSessionFactory);
+			
 			ServletContext sc = event.getServletContext();
 			
 			// 절대경로 호출
 			System.out.println(sc.getRealPath(sc.getInitParameter("contextConfigLocation")));
 			String propertiesPath = 
 					sc.getRealPath(sc.getInitParameter("contextConfigLocation"));
-			applicationContext = new ApplicationContext(propertiesPath);
+			
+			applicationContext.prepareObjectsByProperties(propertiesPath);
+			applicationContext.prepareObjectsByAnnotation("spms");
+			applicationContext.injectDependency();
 			
 		} catch (Throwable e) {
 			e.printStackTrace();
