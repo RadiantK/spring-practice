d53d31de42bd5d36c7ce44e566d7c2e88b015544
RadiantK <pps8853@gmail.com>
2022-05-22 11:10:24 +0900

    ProjectDao 인터페이스 수정
    selectList메소드에 파라미터로 Map객체를 받음

--- workbook-mybatis/src/main/java/spms/dao/ProjectDao.java
+++ workbook-mybatis/src/main/java/spms/dao/ProjectDao.java
@@ -1,11 +1,12 @@
 package spms.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import spms.dto.Project;
 
 public interface ProjectDao {
-	List<Project> selectList();
+	List<Project> selectList(Map<String, Object> paramMap);
 	int insert(Project project);
 	Project selectOne(int no);
 	int update(Project project);
