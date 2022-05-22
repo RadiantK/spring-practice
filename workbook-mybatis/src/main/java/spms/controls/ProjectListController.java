c3e8695336a604477ed89005c56ae5e9083a929b
RadiantK <pps8853@gmail.com>
2022-05-22 11:08:15 +0900

    프로젝트 목록을 출력하는 컨트롤러 수정
    프로젝트 목록을 출력하는 컨트롤러 수정
    selectList메소드의 인자로 Map 객체를 받음

--- workbook-mybatis/src/main/java/spms/controls/ProjectListController.java
+++ workbook-mybatis/src/main/java/spms/controls/ProjectListController.java
@@ -1,23 +1,37 @@
 package spms.controls;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import spms.annotation.Component;
+import spms.bind.DataBinding;
 import spms.dao.ProjectDao;
 
 @Component("/project/list.do")
-public class ProjectListController implements Controller{
+public class ProjectListController implements Controller, DataBinding{
 	ProjectDao projectDao;
 	
 	public ProjectListController setMemberDao(ProjectDao projectDao) {
 		this.projectDao = projectDao;
-		return this;	
+		return this;
+	}
+	
+	@Override
+	public Object[] getDataBinders() {
+		return new Object[] {
+			"orderCond", String.class
+		};
 	}
 	
 	@Override
 	public String execute(Map<String, Object> model) throws Exception {
-		model.put("projects", projectDao.selectList());
+		Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("orderCond", model.get("orderCond"));
+		
+		model.put("projects", projectDao.selectList(paramMap));
 		
 		return "/project/projectList.jsp";
 	}
+
+	
 }
