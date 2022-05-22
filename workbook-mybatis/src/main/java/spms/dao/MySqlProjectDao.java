65715cfb78416f402a0b0faf8ccb38269ca6ac6c
RadiantK <pps8853@gmail.com>
2022-05-22 11:08:56 +0900

    MySqlDao 수정
    프로젝트를 수정할 때 입력된 값들만  update문이 실행할 수 있도록 수정

--- workbook-mybatis/src/main/java/spms/dao/MySqlProjectDao.java
+++ workbook-mybatis/src/main/java/spms/dao/MySqlProjectDao.java
@@ -1,6 +1,8 @@
 package spms.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -23,11 +25,11 @@ public class MySqlProjectDao implements ProjectDao{
 	}
 	
 	@Override
-	public List<Project> selectList() {
+	public List<Project> selectList(Map<String, Object> paramMap) {
 		// sqlSession객체 생성
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		try {
-			return sqlSession.selectList("spms.dao.ProjectDao.selectList");
+			return sqlSession.selectList("spms.dao.ProjectDao.selectList", paramMap);
 		}finally {
 			sqlSession.close();
 		}
@@ -62,7 +64,35 @@ public class MySqlProjectDao implements ProjectDao{
 		// sqlSession객체 생성
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		try {
-			int n = sqlSession.update("spms.dao.ProjectDao.update", project);
+			Project original = sqlSession.selectOne(
+					"spms.dao.ProjectDao.selectOne", project.getNo());
+			
+			Map<String, Object> paramMap = new HashMap<>();
+			
+			if(!project.getTitle().equals(original.getTitle())) {
+				paramMap.put("title", project.getTitle());
+			}
+			if(!project.getContent().equals(original.getContent())) {
+				paramMap.put("content", project.getContent());
+			}
+			if(!project.getStartDate().equals(original.getStartDate())) {
+				paramMap.put("startDate", project.getStartDate());
+			}
+			if(!project.getEndDate().equals(original.getEndDate())) {
+				paramMap.put("endDate", project.getEndDate());
+			}
+			if(project.getState() != original.getState()) {
+				paramMap.put("state", project.getState());
+			}
+			if(!project.getTags().equals(original.getTags())) {
+				paramMap.put("tags", project.getTags());
+			}
+			
+			if(paramMap.size() > 0) {
+				paramMap.put("no", project.getNo());
+			}
+			
+			int n = sqlSession.update("spms.dao.ProjectDao.update", paramMap);
 			sqlSession.commit();
 			return n;
 		}finally {
