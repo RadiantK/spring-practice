e22c2e494b07f0ffd660b0abd3d9cbe8328b5c8c
RadiantK <pps8853@gmail.com>
2022-05-23 21:47:13 +0900

    xml빈 설정 필드값으로 객체를 갖는 빈의 개별인스턴스 주입
    xml빈 설정 필드값으로 객체를 갖는 빈의 개별인스턴스 주입

---
+++ workbook-spring01/lib/src/main/java/exam/test08/Car.java
@@ -0,0 +1,54 @@
+package exam.test08;
+
+public class Car {
+	private String model; // 모델명
+	private Engine engine; // 엔진
+	private Tire[] tires;
+	
+	public Car() {}
+
+	public Car(String model, Engine engine) {
+		this.model = model;
+		this.engine = engine;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public Engine getEngine() {
+		return engine;
+	}
+
+	public void setEngine(Engine engine) {
+		this.engine = engine;
+	}
+
+	public Tire[] getTires() {
+		return tires;
+	}
+
+	public void setTires(Tire[] tires) {
+		this.tires = tires;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder carInfo = new StringBuilder();
+		carInfo.append("[Car:" + model);
+		carInfo.append("\n " + engine.toString());
+		
+		if(tires != null) {
+			for(Tire tire : tires) {
+				carInfo.append("\n " + tire.toString());
+			}
+		}
+		
+		carInfo.append("\n]");
+		return carInfo.toString();
+	}
+}
