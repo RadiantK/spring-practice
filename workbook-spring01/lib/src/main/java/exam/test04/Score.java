ec68827b1f1da539703f53f4b33f82fa7c12fae2
RadiantK <pps8853@gmail.com>
2022-05-23 21:43:34 +0900

    xml빈 설정 생성자 값 설정
    xml빈 설정 constructor-argument값 주입

---
+++ workbook-spring01/lib/src/main/java/exam/test04/Score.java
@@ -0,0 +1,63 @@
+package exam.test04;
+
+public class Score {
+	private String name;
+	private double kor;
+	private double eng;
+	private double math;
+	
+	public Score() { }
+	
+	public Score(String name, double kor, double eng, double math) {
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+	
+	public double average() {
+		return sum() / (double)3;
+	}
+	
+	public double sum() {
+		return kor + eng + math;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getKor() {
+		return kor;
+	}
+	
+	public void setKor(double kor) {
+		this.kor = kor;
+	}
+
+	public double getEng() {
+		return eng;
+	}
+
+	public void setEng(double eng) {
+		this.eng = eng;
+	}
+
+	public double getMath() {
+		return math;
+	}
+
+	public void setMath(double math) {
+		this.math = math;
+	}
+
+	@Override
+	public String toString() {
+		return "Score [name=" + name + ", kor=" + kor + ", eng=" + eng + ", math=" + math + "]";
+	}
+
+}
