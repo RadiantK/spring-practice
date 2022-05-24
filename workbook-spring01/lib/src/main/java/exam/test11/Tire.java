51e05b431cf553226e44b68753b1d29242d8d88f
RadiantK <pps8853@gmail.com>
2022-05-24 23:40:46 +0900

    펙토리 메소드를 사용한 간접 객체생성
    펙토리 메소드를 사용한 간접 객체생성

---
+++ workbook-spring01/lib/src/main/java/exam/test11/Tire.java
@@ -0,0 +1,49 @@
+package exam.test11;
+
+import java.util.Date;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+public class Tire {
+	private String maker; // 제조사
+	private Properties spec; // 규격
+	private Date createdDate;
+
+	public String getMaker() {
+		return maker;
+	}
+
+	public void setMaker(String maker) {
+		this.maker = maker;
+	}
+
+	public Properties getSpec() {
+		return spec;
+	}
+
+	public void setSpec(Properties spec) {
+		this.spec = spec;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder specInfo = new StringBuilder();
+		if(spec != null) {
+			for(Entry<Object, Object> entry : spec.entrySet()){
+				specInfo.append(entry.getKey() + ":" + entry.getValue()+",");
+			}
+		}
+		return "[Tire:" + maker + "," + specInfo.toString() + 
+				((createdDate != null) ? 
+						(","+createdDate.toString()) : "") + "]";
+	}
+	
+}
---
+++ workbook-spring01/lib/src/main/java/exam/test11/TireFactory.java
@@ -0,0 +1,57 @@
+package exam.test11;
+
+import java.text.SimpleDateFormat;
+import java.util.Properties;
+
+public class TireFactory {
+	public static Tire createTire(String maker) {
+		if(maker.equals("Hankook")) {
+			return createHanKookTire();
+		}else {
+			return createKumhoTire();
+		}
+	}
+
+	private static Tire createHanKookTire() {
+		Tire tire = new Tire();
+		tire.setMaker("Hankook");
+		
+		Properties spec = new Properties();
+		spec.setProperty("width", "205");
+		spec.setProperty("ratio", "65");
+		spec.setProperty("rim.diameter", "14");
+		tire.setSpec(spec);
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		
+		try {
+			tire.setCreatedDate(dateFormat.parse("2020-05-05"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return tire;
+	}
+	
+	private static Tire createKumhoTire() {
+		Tire tire = new Tire();
+		tire.setMaker("Kumho");
+		
+		Properties spec = new Properties();
+		spec.setProperty("width", "185");
+		spec.setProperty("ratio", "75");
+		spec.setProperty("rim.diameter", "16");
+		tire.setSpec(spec);
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		
+		try {
+			tire.setCreatedDate(dateFormat.parse("2020-04-05"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return tire;
+	}
+
+}
