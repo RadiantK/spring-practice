5cefed631ff36b72dc5025d108f603382ce17591
RadiantK <pps8853@gmail.com>
2022-05-25 23:49:28 +0900

    @Component 사용
    @Component 사용

---
+++ workbook-spring01/lib/src/main/java/exam/test21/Test.java
@@ -0,0 +1,28 @@
+package exam.test21;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ctx =
+				new ClassPathXmlApplicationContext("exam/test21/beans.xml");
+		
+		Car car = (Car) ctx.getBean("car");
+		
+		Engine engine = (Engine) ctx.getBean("engine");
+		engine.setMaker("Hyndai");
+		engine.setCc(1997);
+		
+		if(car != null) {
+			System.out.println("car != null");
+		}
+		
+		if(engine != null) {
+			System.out.println("engine != null");
+		}
+		
+		System.out.println(car);
+
+		ctx.close();
+	}
+}
