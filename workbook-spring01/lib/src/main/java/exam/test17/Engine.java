8df3e744c9232ee4dd377d8def74d35d75c2e92a
RadiantK <pps8853@gmail.com>
2022-05-25 23:47:43 +0900

    @Autowired 사용
    @Autowired 사용

---
+++ workbook-spring01/lib/src/main/java/exam/test17/Engine.java
@@ -0,0 +1,34 @@
+package exam.test17;
+
+public class Engine {
+	private String maker; // 제조사
+	private int cc; // 배기량
+	
+	public Engine() {}
+	
+	public Engine(String maker) {
+		this.maker = maker;
+	}
+
+	public String getMaker() {
+		return maker;
+	}
+
+	public void setMaker(String maker) {
+		this.maker = maker;
+	}
+
+	public int getCc() {
+		return cc;
+	}
+
+	public void setCc(int cc) {
+		this.cc = cc;
+	}
+
+	@Override
+	public String toString() {
+		return "Engine [maker=" + maker + ", cc=" + cc + "]";
+	}
+	
+}
