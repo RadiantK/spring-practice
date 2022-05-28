24be1b3132da69d3df07ae4fee819981f13accb7
RadiantK <pps8853@gmail.com>
2022-05-28 16:01:02 +0900

    사용하지않는 패키지 및 클래스 제거
    사용하지않는 패키지 및 클래스 제거

--- workbook-spring02/lib/src/main/java/spms/annotation/Component.java
+++
@@ -1,9 +0,0 @@
-package spms.annotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME) // 어노테이션을 런타임시까지 사용
-public @interface Component {
-	String value() default ""; // 객체 이름을 저장하는 용도로 사용할 메소드
-}
